e9a765a2ff5306d7d8a39acb1667a26255d2c6a4
victorsantosdev <viic.santos@gmail.com>
2016-08-28 21:37:52 -0300

    apagando a pasta web

--- Web_Development/backup_mamp/mamp/captcha.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$img = imagecreate(60, 15);
-$fundo = imagecolorallocate($img, 255, 255, 0);
-$fonte = imagecolorallocate($img, 0, 0, 0);
-imagestring($img, 5, 0, 0, ''.$_SESSION['captcha'].'', $fonte);
-header("Content-type: image/jpeg");
-imagejpeg($img);
-?>
